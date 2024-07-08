ba7fe052681e89c50dd732cf03731ce2ab499265
azkarullhassan <azkarullhassan7@gmail.com>
2024-07-08 23:10:02 +0500

    latest version 2.0

--- app/page.tsx
+++ app/page.tsx
@@ -1,19 +1,19 @@
 import { SiX } from "react-icons/si";
-import First from "./components/First";
-import Five from "./components/Five";
-import Four from "./components/Four";
-import Hero from "./components/Hero";
-import Second from "./components/Second";
-import Third from "./components/Third";
-import Navbar from "./components/navbar";
-import Top from "./components/top";
-import Six from "./components/Six";
-import Seven from "./components/Seven";
-import Eight from "./components/Eight";
-import Nine from "./components/Nine";
-import Ten from "./components/Ten";
-import Eleven from "./components/Eleven";
-import Footer from "./components/Footer";
+import First from "../components/First";
+import Five from "../components/Five";
+import Four from "../components/Four";
+import Hero from "../components/Hero";
+import Second from "../components/Second";
+import Third from "../components/Third";
+import Navbar from "../components/navbar";
+import Top from "../components/top";
+import Six from "../components/Six";
+import Seven from "../components/Seven";
+import Eight from "../components/Eight";
+import Nine from "../components/Nine";
+import Ten from "../components/Ten";
+import Eleven from "../components/Eleven";
+import Footer from "../components/Footer";
 
 
 export default function Home() {
